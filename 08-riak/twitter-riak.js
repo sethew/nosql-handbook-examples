11f16e0ef676a8a39772d8bf3dbfccc4408f0822
Seth Sweep <seth.sweep@gmail.com>
2012-02-15 21:49:23 -0600

    fixed require to match vendor directory

--- 08-riak/twitter-riak.js
+++ 08-riak/twitter-riak.js
@@ -1,11 +1,11 @@
 var riak = require('./vendor/riak-js').getClient();
 
-var twitter = require('ntwitter');
+var twitter = require('./vendor/ntwitter');
 var twit = new twitter({
   consumer_key: 'YOUR_CONSUMER_KEY',
   consumer_secret: 'YOUR_CONSUMER_SECRET',
   access_token_key: 'YOUR_ACCESS_TOKEN_KEY',
-  access_token_secret: 'YOUR_ACCESS_TOKEN_SECRENT'
+  access_token_secret: 'YOUR_ACCESS_TOKEN_SECRET'
 });
 
 twit.stream('statuses/filter', {'track':'justin bieber'}, function(stream) {
