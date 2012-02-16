0cb1080c46aee88de44db1d5ef484e9bebbd8087
Seth Sweep <seth.sweep@gmail.com>
2012-02-15 21:36:25 -0600

    Updated twitter-riak.js to use ntwitter, an updated version of twitter-node. See https://github.com/AvianFlu/ntwitter for more documentation. (Specifically note the Setup API for new instructions on connecting to the Twitter API.)

--- 08-riak/twitter-riak.js
+++ 08-riak/twitter-riak.js
@@ -1,26 +1,25 @@
-var TwitterNode = require('./vendor/twitter-node').TwitterNode;
 var riak = require('./vendor/riak-js').getClient();
-var sys = require('sys');
-var twitter = new TwitterNode();
-twitter.user = process.env['TWITTER_USER'];
-twitter.password = process.env['TWITTER_PASSWORD'];
 
-twitter.track('justin bieber');
-
-twitter.addListener('error', function(error) {
-  console.log(error.message);
+var twitter = require('ntwitter');
+var twit = new twitter({
+  consumer_key: 'YOUR_CONSUMER_KEY',
+  consumer_secret: 'YOUR_CONSUMER_SECRET',
+  access_token_key: 'YOUR_ACCESS_TOKEN_KEY',
+  access_token_secret: 'YOUR_ACCESS_TOKEN_SECRENT'
 });
 
-twitter.addListener('tweet', function(tweet) {
-  var tweetObject = {user: tweet.user.screen_name, tweet: tweet.text, tweeted_at: tweet.created_at}
-  var links = [];
-  var key = tweet.id_str;
-  if (tweet.in_reply_to_status_id_str != null) {
-    console.log("Tweet " + key + " is a reply to " + tweet.in_reply_to_status_id_str);
-    links.push({tag: 'in_reply_to', bucket: 'tweets', key: tweet.in_reply_to_status_id_str});
-  }
-  riak.save('tweets', key, tweetObject, {links: links}, function(error) {
-    if (error != null)
-      console.log(error);
+twit.stream('statuses/filter', {'track':'justin bieber'}, function(stream) {
+  stream.on('data', function (tweet) {
+    var tweetObject = {user: tweet.user.screen_name, tweet: tweet.text, tweeted_at: tweet.created_at}
+    var links = [];
+    var key = tweet.id_str;
+    if (tweet.in_reply_to_status_id_str != null) {
+      console.log("Tweet " + key + " is a reply to " + tweet.in_reply_to_status_id_str);
+      links.push({tag: 'in_reply_to', bucket: 'tweets', key: tweet.in_reply_to_status_id_str});
+    }    
+    riak.save('tweets', key, tweetObject, {links: links}, function(error) {
+      if (error != null)
+        console.log(error);
+    });
   });
-}).stream();
+});
